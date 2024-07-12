372ee49d661243eef0ae00e56433ef5c96aa710f
timohuber <timo.huber@bluemail.ch>
2024-07-12 17:43:20 +0200

    Feature/render mermaid (#2)
    Ad pipeline stage 
    
    ---------
    
    Co-authored-by: Timo Huber <timo.huber@econ.uzh.ch>

--- scripts/render-images.sh
+++ scripts/render-images.sh
@@ -1,11 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 
 input_dir="diagrams"
 output_dir="docs/images"
 
+command_path=$( [ -z "$CI" ] && echo "." || echo "/home/mermaidcli" )
+config_path=$( [ -z "$CI" ] && echo "." || echo "")
+
+mkdir -p "$output_dir"
+
+echo "Start rendering mermaid diagrams"
+
 for file in "$input_dir"/*.mmd; do
+  echo "rendering $file"
+
   base_name=$(basename "$file" .mmd)
-  ./node_modules/.bin/mmdc -p puppeteer-config.json -i "$file" -o "$output_dir/${base_name}.svg" -b transparent
+  "$command_path/node_modules/.bin/mmdc" -p "$config_path/puppeteer-config.json" -i "$file" -o "$output_dir/${base_name}.svg" -b transparent
 done
 
 echo "Rendering mermaid diagrams completed."
