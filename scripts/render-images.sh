6d466419d8930a627851d5d3447bff6d01baedb1
Timo Huber <timo.huber@econ.uzh.ch>
2024-07-12 16:55:44 +0200

    WIP

--- scripts/render-images.sh
+++ scripts/render-images.sh
@@ -3,7 +3,10 @@
 input_dir="diagrams"
 output_dir="docs/images"
 
+echo "Start rendering mermaid diagrams"
+
 for file in "$input_dir"/*.mmd; do
+  echo "rendering $file"
   base_name=$(basename "$file" .mmd)
   ./node_modules/.bin/mmdc -p puppeteer-config.json -i "$file" -o "$output_dir/${base_name}.svg" -b transparent
 done
