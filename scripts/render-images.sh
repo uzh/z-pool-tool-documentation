c87123575e52dc9454ab1d9c3c65bbc3bc134024
Timo Huber <timo.huber@econ.uzh.ch>
2024-07-12 17:24:58 +0200

    update script file

--- scripts/render-images.sh
+++ scripts/render-images.sh
@@ -3,12 +3,16 @@
 input_dir="diagrams"
 output_dir="docs/images"
 
+command_path=$( [ -z "$CI" ] && echo "." || echo "/home/mermaidcli" )
+config_path=$( [ -z "$CI" ] && echo "." || echo "")
+
 echo "Start rendering mermaid diagrams"
 
 for file in "$input_dir"/*.mmd; do
   echo "rendering $file"
+
   base_name=$(basename "$file" .mmd)
-  ./node_modules/.bin/mmdc -p puppeteer-config.json -i "$file" -o "$output_dir/${base_name}.svg" -b transparent
+  "$command_path/node_modules/.bin/mmdc" -p "$config_path/puppeteer-config.json" -i "$file" -o "$output_dir/${base_name}.svg" -b transparent
 done
 
 echo "Rendering mermaid diagrams completed."
